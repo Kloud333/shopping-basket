4bef8391ac55c94e4571e41754dc1345877a9ce8
Volodymyr Klekot <klekotvr@gmail.com>
2019-04-11 23:36:15 +0300

    - updated migration

--- src/Migrations/Version20190411203449.php
+++ src/Migrations/Version20190411203449.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190411161126 extends AbstractMigration
+final class Version20190411203449 extends AbstractMigration
 {
     public function getDescription() : string
     {
