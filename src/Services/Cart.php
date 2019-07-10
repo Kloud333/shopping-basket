c24a595f950e7f07b040038fae39e6431a502265
Volodymyr Klekot <Volodymyr.Klekot@clarivate.com>
2019-07-10 13:00:57 +0300

    - updated discount usage algorith

--- src/Services/Cart.php
+++ src/Services/Cart.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Services\Discount\Discount;
+use App\Services\Discount\LoyaltyCardDiscount;
 use App\Services\Discount\TotalFixedDiscount;
 use App\Services\Discount\SecondProductDiscount;
 
@@ -11,11 +12,12 @@ class Cart
     /**
      * @param $cartProducts
      * @param $customerId
+     * @param $customerDiscounts
      * @return float
      */
-    public function calculateTotal($cartProducts, $customerId)
+    public function calculateTotal($cartProducts, $customerId, $customerDiscounts)
     {
-        $discount = $this->getUserDiscounts($cartProducts, $customerId);
+        $discount = $this->getUserDiscounts($cartProducts, $customerId, $customerDiscounts);
 
         return $discount->calculate($cartProducts, 0);
     }
@@ -23,17 +25,20 @@ class Cart
     /**
      * @param $cartProducts
      * @param $customerId
+     * @param $customerDiscounts
      * @return TotalFixedDiscount|SecondProductDiscount|Discount
      */
-    protected function getUserDiscounts($cartProducts, $customerId)
+    protected function getUserDiscounts($cartProducts, $customerId, $customerDiscounts)
     {
+        $userDiscounts = $this->getAvailableUserDiscounts($customerDiscounts);
+
         $discount = new Discount();
 
-        if (!empty($cartProducts)) {
+        if (in_array('second-product', $userDiscounts)) {
             $discount = new SecondProductDiscount();
         }
 
-        if (reset($cartProducts)['quantity'] >= 2) {
+        if (in_array('fixed-total', $userDiscounts)) {
             $totalFixedDiscount = new TotalFixedDiscount(500, 10);
 
             if (isset($discount)) {
@@ -43,11 +48,31 @@ class Cart
             }
         }
 
+        if (in_array('loyalty-card', $userDiscounts)) {
+            $LoyaltyCardDiscount = new LoyaltyCardDiscount(10);
+
+            if (isset($discount)) {
+                $discount->setNext($LoyaltyCardDiscount);
+            } else {
+                $discount = $LoyaltyCardDiscount;
+            }
+        }
+
         return $discount;
     }
 
-    protected function getAvailableUserDiscounts()
+    /**
+     * @param array $customerDiscounts
+     * @return array
+     */
+    protected function getAvailableUserDiscounts(array $customerDiscounts)
     {
-        //TODO Implement the available discounts for the user
+        $discounts = [];
+
+        foreach ($customerDiscounts as $customerDiscount) {
+            $discounts[] = $customerDiscount['discount'];
+        }
+
+        return $discounts;
     }
 }
